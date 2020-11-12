55ece99d5bc77b50f16c35b16ea1e51031f29319
santoshpavan <santoshpavan.psp@gmail.com>
2020-11-12 11:12:16 -0500

    lock deletion

--- sys/ldelete.c
+++ sys/ldelete.c
@@ -1,6 +1,25 @@
 /* Delete the lock descriptor passed */
 
-int ldelete (int lockdescriptor) {
+int ldelete (int lock) {
+    STATWORD ps;    
+	int	pid;
+	struct	lentry	*lptr;
 
-    return SYSERR;
+	disable(ps);
+	if (isbadsem(lock) || semaph[lock].lstate==SFREE) {
+		restore(ps);
+		return(SYSERR);
+	}
+	lptr = &locks[lock];
+	lptr->lstate = LFREE;
+	if (nonempty(lptr->sqhead)) {
+		while( (pid=getfirst(lptr->lqhead)) != EMPTY)
+		  {
+		    proctab[pid].pwaitret = DELETED;
+		    ready(pid,RESCHNO);
+		  }
+		resched();
+	}
+	restore(ps);
+	return(OK);
 }
